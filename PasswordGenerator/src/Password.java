e9e70ac0f09f7b48d34f36c2cd21a2fe4774abb0
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2020-04-08 11:29:56 +0200

    Final version of Password Generator

--- PasswordGenerator/src/Password.java
+++ PasswordGenerator/src/Password.java
@@ -16,26 +16,29 @@ public class Password {
         }
         return 0;
     }
-    static void numbers(boolean value) {
+    static char numbers(boolean value) {
         Random random = new Random();
         String chars = "1234567890";
         if (value) {
-            char c = chars.charAt(random.nextInt(chars.length()));
+            return chars.charAt(random.nextInt(chars.length()));
         }
+        return 0;
     }
-    static void lowercase(boolean value) {
+    static char lowercase(boolean value) {
         Random random = new Random();
         String chars = "abcdefghijklmnopqrstuwxyz";
         if (value) {
-            char c = chars.charAt(random.nextInt(chars.length()));
+            return chars.charAt(random.nextInt(chars.length()));
         }
+        return 0;
     }
-    static void uppercase(boolean value) {
+    static char uppercase(boolean value) {
         Random random = new Random();
         String chars = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
         if (value) {
-            char c = chars.charAt(random.nextInt(chars.length()));
+            return chars.charAt(random.nextInt(chars.length()));
         }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -48,25 +51,44 @@ public class Password {
         if (scanner.next().equalsIgnoreCase("y")) {
             symbols = true;
         }
-//        System.out.println("2. Include Numbers: (e.g. 12345). (Y) or (N)");
-//        if (scanner.next().equalsIgnoreCase("y")) {
-//            numbers = true;
-//        }
-//        System.out.println("3. Include Lowercase Characters: (e.g. abcde). (Y) or (N)");
-//        if (scanner.next().equalsIgnoreCase("y")) {
-//            lowercase = true;
-//        }
-//        System.out.println("4. Include Uppercase Characters: (e.g. ABCDE). (Y) or (N)");
-//        if (scanner.next().equalsIgnoreCase("y")) {
-//            uppercase = true;
-//        }
-
-        for (int i = 0; i <howLong; i++) {
-            if(password.length() <= howLong) {
-                password += symbols(symbols);
-            }
+        System.out.println("2. Include Numbers: (e.g. 12345). (Y) or (N)");
+        if (scanner.next().equalsIgnoreCase("y")) {
+            numbers = true;
         }
-        System.out.println("Your Password is: " + password);
+        System.out.println("3. Include Lowercase Characters: (e.g. abcde). (Y) or (N)");
+        if (scanner.next().equalsIgnoreCase("y")) {
+            lowercase = true;
+        }
+        System.out.println("4. Include Uppercase Characters: (e.g. ABCDE). (Y) or (N)");
+        if (scanner.next().equalsIgnoreCase("y")) {
+            uppercase = true;
+        }
+
+        while (password.length() < howLong) {
+                char s = symbols(symbols);
+                if (s != 0){
+                    password += s;
+                }
+                if (password.length() < howLong) {
+                    char n = numbers(numbers);
+                    if (n != 0) {
+                        password += n;
+                    }
+                }
+                if (password.length() < howLong) {
+                    char l = lowercase(lowercase);
+                    if(l != 0) {
+                        password += l;
+                    }
+                }
+                if (password.length() < howLong) {
+                    char u = uppercase(uppercase);
+                    if(u != 0) {
+                        password += u;
+                    }
+                }
+            }
+            System.out.println("Your Password is: " + password);
     }
 }
 
